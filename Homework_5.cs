f4573ebb9c8070a915da5a47354fd7aeb471e0fe
MrSadyga <127867283+MrSadyga@users.noreply.github.com>
2023-03-15 20:14:38 +0500

    Add files via upload

---
+++ Homework_5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Homework_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string surname;
+            string mesage;
+
+            name = ("Александр");
+            surname = ("Владимирович");
+            mesage = name + ' ' + surname;
+
+            Console.WriteLine($"{surname} {name}");
+            Console.WriteLine($"{mesage}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework_6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesCount = 52;
+            int countPerColumn = 3;
+
+            int fullRowsCount = picturesCount / countPerColumn;
+            int overflowCount = picturesCount % countPerColumn;
+
+            Console.WriteLine($"Рядов занято: {fullRowsCount}");
+            Console.WriteLine($"Остаток картинок: {overflowCount} ");
+            Console.ReadLine();
+        }
+    }
+}
