20676786c88dc77a894ebf8d4607b340885ae90b
MrSadyga <127867283+MrSadyga@users.noreply.github.com>
2023-03-17 14:57:55 +0500

    Add files via upload

---
+++ Homework_5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Homework_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string patronymic;
+            string templateName;
+
+            name = ("Владимирович");
+            patronymic = ("Александр");
+
+            Console.WriteLine($"{name} {patronymic}");
+
+            templateName = name;
+            name = patronymic;
+            patronymic = templateName;
+
+            Console.WriteLine($"{name} {patronymic}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework_7.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Homework_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string shopName = ("Легенда");
+            int goldCoins;
+            int priceForCrystal = 10;
+            int numberOfCrystals;
+            int valueOfCrystals;
+            int remainingGold;
+            int sum;
+
+            Console.WriteLine($"Добро пожаловать в магазин {shopName}.");
+            Console.WriteLine("У нас вы можете приобрести кристаллы.");
+            Console.WriteLine("Укажите какое колличество золота у вас имеется?");
+
+            goldCoins = Convert.ToInt32 (Console.ReadLine());
+            sum = goldCoins / priceForCrystal;
+
+            Console.WriteLine($"У вас {goldCoins} золота цена за один кристалл {priceForCrystal} золота, \n" +
+                $"Вы можете приобрести {sum} кристаллов");
+            Console.WriteLine("Укажите сколько кристаллов вы бы хотели приобрести");
+            numberOfCrystals = Convert.ToInt32(Console.ReadLine());
+            valueOfCrystals = priceForCrystal * numberOfCrystals;
+            remainingGold = goldCoins - valueOfCrystals;
+
+            Console.WriteLine($"Колличество ваших кристаллов составляет {numberOfCrystals},\n" +
+                $"Затраты золота составили {valueOfCrystals},\n" +
+                $"Остаток золота в кошельке {remainingGold},\n" +
+                $"Приходите еще, мы будем рады видеть вас в нашем магазине {shopName}!");
+            Console.ReadKey();
+        }
+    }
+}
